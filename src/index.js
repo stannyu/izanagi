48a7912d5ead1977a790beb542e580809d77d46e
stannyu <stanislau.yudzitski@speakap.nl>
2020-09-25 10:49:41 +0200

    Complete MVC setup, working state

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+// here I am importing both styles - for library and app
+// but in real world scenario this would be only library specific styles
 import './assets/app-styles.scss';
 import './assets/editor-styles.scss';
 
@@ -6,10 +8,23 @@ import { Model } from './emails-editor/model';
 import { View } from './emails-editor/view';
 
 class EmailsEditor {
-  constructor() {
-    const controller = new Controller();
-    const model = new Model();
-    const view = new View();
+  EMAILS_EDITOR;
+
+  constructor(container, options = {}) {
+    // best place to handle instantiation errors
+    if (!container || container.hasChildNodes()) {
+      throw new Error('EmailsEditor require valid DOM element without content.');
+    }
+
+    this.EMAILS_EDITOR = new Controller(container, options, new Model(container, options), new View());
+  }
+
+  handleAddEmails(element) {
+    element.addEventListener('click', () => this.EMAILS_EDITOR.addRandomEmail(this));
+  }
+
+  handleEmailsCount(element) {
+    element.addEventListener('click', () => this.EMAILS_EDITOR.getValidEmailsCount());
   }
 }
 
