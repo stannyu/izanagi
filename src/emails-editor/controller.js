de9a50cac65a3ac8a4dd7b1cea08f01839978795
stannyu <stanislau.yudzitski@speakap.nl>
2020-09-25 10:44:21 +0200

    basic MVC structure setup

---
+++ src/emails-editor/controller.js
@@ -0,0 +1,5 @@
+export class Controller {
+  constructor() {
+    console.log("controller instantiated");
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
-console.log("index loaded");
+import './assets/app-styles.scss';
+import './assets/editor-styles.scss';
+
+import { Controller } from './emails-editor/controller';
+import { Model } from './emails-editor/model';
+import { View } from './emails-editor/view';
+
+class EmailsEditor {
+  constructor() {
+    const controller = new Controller();
+    const model = new Model();
+    const view = new View();
+  }
+}
+
+window.EmailsEditor = EmailsEditor;
+export default EmailsEditor;
